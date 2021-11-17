87ebe88ab89b72bfefebb9cf434ee957d8450d9c
aanper <mail@s3f.ru>
2021-11-17 22:48:38 +0300

    speedup all html

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(dirname $SCRIPT)
 
 export DISPLAY=:0
 
-cd /boot/bydefault/all-htmls/
+cd /boot/bydefault/html/
 python -m http.server &
 sleep 2s
-chromium --kiosk --start-fullscreen --incognito --noerrdialogs --disable-features=Translate --no-first-run --disable-infobars --use-fake-ui-for-media-stream http://localhost:8000/ga-mimicry-cam-2.html
+chromium --kiosk --start-fullscreen --incognito --noerrdialogs --disable-features=Translate --no-first-run --disable-infobars --use-fake-ui-for-media-stream --window-position=0,0 --window-size=800,480 http://localhost:8000/ga-mimicry-cam-2.html
