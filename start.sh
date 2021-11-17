1e95066170d809e8a5c87ae494b401e494a8bc58
aanper <mail@s3f.ru>
2021-11-17 22:19:52 +0300

    speedup and fix start

--- start.sh
+++ start.sh
@@ -3,4 +3,9 @@
 SCRIPT=$(readlink -f $0)
 SCRIPT_DIR=$(dirname $SCRIPT)
 
-chromium --kiosk --start-fullscreen --incognito --noerrdialogs --disable-features=Translate --no-first-run --disable-infobars --use-fake-ui-for-media-stream /boot/bydefault/all-htmls/ga-mimicry-cam-1.html
+export DISPLAY=:0
+
+cd /boot/bydefault/all-htmls/
+python -m http.server &
+sleep 2s
+chromium --kiosk --start-fullscreen --incognito --noerrdialogs --disable-features=Translate --no-first-run --disable-infobars --use-fake-ui-for-media-stream http://localhost:8000/ga-mimicry-cam-2.html
